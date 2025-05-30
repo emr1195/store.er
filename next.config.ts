1efb9009428cc10f23836f774a348d18f3f56c2b
enoc mena <enockm1120@gmail.com>
2025-05-30 15:20:03 -0500

    next.config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: "export", // <--- esto habilita next export
+  
   images: {
     unoptimized: true, // necesario para next export si usas imágenes
     remotePatterns: [
