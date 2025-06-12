b687f5b06c5c1bc3e4c47957e5333ae523a4b4d0
enoc mena <enockm1120@gmail.com>
2025-06-12 15:24:40 -0500

    new token

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -2,11 +2,10 @@ import { createClient } from "next-sanity";
 import { apiVersion, dataset, projectId } from "../env";
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: "2023-08-01",
-  token: process.env.NEXT_PUBLIC_SANITY_TOKEN, // Añade esta línea
-  useCdn: false, // Importante para datos en tiempo real,
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: false,
   stega: {
     studioUrl:
       process.env.NODE_ENV === "production"
