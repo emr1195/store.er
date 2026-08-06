ea56c6c0fa84347051ee04f3098345714db1c39f
enoc mena <enockm1120@gmail.com>
2026-08-06 14:39:47 -0500

    fix: clear paid items from cart

--- components/ClearCartOnSuccess.tsx
+++ components/ClearCartOnSuccess.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { useEffect } from "react";
-import useCartStore from "@/store";
+import useCartStore, { type PurchasedCartItem } from "@/store";
 
-export default function ClearCartOnSuccess() {
-  const resetCart = useCartStore((state) => state.resetCart);
-  useEffect(() => resetCart(), [resetCart]);
+export default function ClearCartOnSuccess({
+  checkoutId,
+  items,
+}: {
+  checkoutId: string;
+  items: PurchasedCartItem[];
+}) {
+  const removePurchasedItems = useCartStore((state) => state.removePurchasedItems);
+
+  useEffect(() => {
+    removePurchasedItems(checkoutId, items);
+  }, [checkoutId, items, removePurchasedItems]);
+
   return null;
 }
